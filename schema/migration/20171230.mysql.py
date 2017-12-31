9de0008d131bcf40c4d90b0fc17f93df243dcd72
spl0k <alban.feron@gmail.com>
2017-12-31 16:24:46 +0100

    Fixed mysql migration removing not null constraints

--- schema/migration/20171230.mysql.py
+++ schema/migration/20171230.mysql.py
@@ -26,20 +26,26 @@ parser.add_argument('database')
 parser.add_argument('-H', '--host', default = 'localhost', help = 'default: localhost')
 args = parser.parse_args()
 
-def process_table(connection, table, fields):
-    to_update = { field: set() for field in fields }
+def process_table(connection, table, fields, nullable_fields = ()):
+    to_update = { field: set() for field in fields + nullable_fields }
 
     c = connection.cursor()
-    c.execute('SELECT {1} FROM {0}'.format(table, ','.join(fields)))
+    c.execute('SELECT {1} FROM {0}'.format(table, ','.join(fields + nullable_fields)))
     for row in c:
-        for field, value in zip(fields, row):
+        for field, value in zip(fields + nullable_fields, row):
             if value is None or not isinstance(value, basestring):
                 continue
             to_update[field].add(value)
 
     for field, values in to_update.iteritems():
+        if not values:
+            continue
         sql = 'UPDATE {0} SET {1}=%s WHERE {1}=%s'.format(table, field)
         c.executemany(sql, map(lambda v: (UUID(v).bytes, v), values))
+    for field in fields:
+        sql = 'ALTER TABLE {0} MODIFY {1} BINARY(16) NOT NULL'.format(table, field)
+        c.execute(sql)
+    for field in nullable_fields:
         sql = 'ALTER TABLE {0} MODIFY {1} BINARY(16)'.format(table, field)
         c.execute(sql)
 
@@ -49,11 +55,11 @@ filterwarnings('ignore', category = provider.Warning)
 conn =  provider.connect(host = args.host, user = args.username, passwd = args.password, db = args.database)
 conn.cursor().execute('SET FOREIGN_KEY_CHECKS = 0')
 
-process_table(conn, 'folder', ('id', 'parent_id'))
+process_table(conn, 'folder', ('id',), ('parent_id',))
 process_table(conn, 'artist', ('id',))
 process_table(conn, 'album', ('id', 'artist_id'))
 process_table(conn, 'track', ('id', 'album_id', 'artist_id', 'root_folder_id', 'folder_id'))
-process_table(conn, 'user', ('id', 'last_play_id'))
+process_table(conn, 'user', ('id',), ('last_play_id',))
 process_table(conn, 'client_prefs', ('user_id',))
 process_table(conn, 'starred_folder', ('user_id', 'starred_id'))
 process_table(conn, 'starred_artist', ('user_id', 'starred_id'))
