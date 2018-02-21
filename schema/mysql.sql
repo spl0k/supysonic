3005e529a2ac8552791b5d508af1d7b9dda0f09e
spl0k <alban.feron@gmail.com>
2018-02-21 21:55:00 +0100

    Fixed MySQL schema using broken UTF-8

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -7,18 +7,18 @@ CREATE TABLE folder (
 	has_cover_art BOOLEAN NOT NULL,
 	last_scan INTEGER NOT NULL,
 	parent_id BINARY(16) REFERENCES folder
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE artist (
 	id BINARY(16) PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE album (
 	id BINARY(16) PRIMARY KEY,
 	name VARCHAR(256) NOT NULL,
 	artist_id BINARY(16) NOT NULL REFERENCES artist
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE track (
 	id BINARY(16) PRIMARY KEY,
@@ -39,7 +39,7 @@ CREATE TABLE track (
 	last_play DATETIME,
 	root_folder_id BINARY(16) NOT NULL REFERENCES folder,
 	folder_id BINARY(16) NOT NULL REFERENCES folder
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE user (
 	id BINARY(16) PRIMARY KEY,
@@ -52,7 +52,7 @@ CREATE TABLE user (
 	lastfm_status BOOLEAN NOT NULL,
 	last_play_id BINARY(16) REFERENCES track,
 	last_play_date DATETIME
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE client_prefs (
 	user_id BINARY(16) NOT NULL,
@@ -60,56 +60,56 @@ CREATE TABLE client_prefs (
 	format VARCHAR(8),
 	bitrate INTEGER,
 	PRIMARY KEY (user_id, client_name)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE starred_folder (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	starred_id BINARY(16) NOT NULL REFERENCES folder,
 	date DATETIME NOT NULL,
 	PRIMARY KEY (user_id, starred_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE starred_artist (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	starred_id BINARY(16) NOT NULL REFERENCES artist,
 	date DATETIME NOT NULL,
 	PRIMARY KEY (user_id, starred_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE starred_album (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	starred_id BINARY(16) NOT NULL REFERENCES album,
 	date DATETIME NOT NULL,
 	PRIMARY KEY (user_id, starred_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE starred_track (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	starred_id BINARY(16) NOT NULL REFERENCES track,
 	date DATETIME NOT NULL,
 	PRIMARY KEY (user_id, starred_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE rating_folder (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	rated_id BINARY(16) NOT NULL REFERENCES folder,
 	rating INTEGER NOT NULL CHECK(rating BETWEEN 1 AND 5),
 	PRIMARY KEY (user_id, rated_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE rating_track (
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	rated_id BINARY(16) NOT NULL REFERENCES track,
 	rating INTEGER NOT NULL CHECK(rating BETWEEN 1 AND 5),
 	PRIMARY KEY (user_id, rated_id)
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE chat_message (
 	id BINARY(16) PRIMARY KEY,
 	user_id BINARY(16) NOT NULL REFERENCES user,
 	time INTEGER NOT NULL,
 	message VARCHAR(512) NOT NULL
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 CREATE TABLE playlist (
 	id BINARY(16) PRIMARY KEY,
@@ -119,5 +119,5 @@ CREATE TABLE playlist (
 	public BOOLEAN NOT NULL,
 	created DATETIME NOT NULL,
 	tracks TEXT
-) DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
