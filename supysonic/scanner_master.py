b4250bbbec41b151f5465b29e7bad2fbea82242c
Taizo Simpson <tts1848@cs.rit.edu>
2018-10-17 19:49:50 -0400

    Added some basic framework for the new ScannerMaster class

---
+++ supysonic/scanner_master.py
@@ -0,0 +1,84 @@
+import multiprocessing
+import secrets
+import threading
+
+from .managers.folder import FolderManager
+from .scanner import Scanner
+
+def create_process():
+    recv, send = multiprocessing.Pipe()
+    process = multiprocessing.Process(target=set_up_in_process, args=(send,))
+    process.start()
+    connection_info = recv.recv()
+    recv.close()
+    return connection_info
+
+def set_up_in_process(conn):
+    authkey = secrets.token_bytes(8)
+    listener = multiprocessing.connection.Listener(authkey=authkey)
+    address = listener.address
+    conn.send((address, authkey))
+
+    sm = ScannerMaster(listener,[]) #TODO: Accept extensions
+    sm.run()
+
+class ScannerMaster():
+    
+    def __init__(self, listener, extensions):
+        self.listener = listener
+        self.extensions = extensions
+        self.to_scan_condition = threading.Condition()
+        self.to_scan = set()
+        self.scan_thread = threading.Thread(target=self._keep_scanning)
+    
+    def run(self):
+        self.scan_thread.start()
+        while True:
+            try:
+                conn = self.listener.accept()
+            except multiprocessing.connection.AuthenticationError:
+                continue
+            listen_thread = threading.Thread(target=self._listen_for_commands, args=(conn,))
+            listen_thread.start()
+
+    def _keep_scanning(self):
+        while True:
+            self.to_scan_condition.acquire()
+            while not len(self.to_scan):
+                self.to_scan_condition.wait()
+            folder = self.to_scan.pop()
+            self.to_scan_condition.release()
+            self._scan_folder(folder)
+
+    def _scan_folder(self, folder_id):
+        scanner = Scanner(extensions = self.extensions)
+        folder = FolderManager.get(id) # TODO: Handle errors (Throws ValueError and ObjectNotFound)
+        scanner.scan(folder) # TODO: Progress callbacks
+        scanner.finish()
+        stats = scanner.stats()
+        if stats.errors:
+            pass # TODO: Handle Errors
+    
+    def _listen_for_commands(self, conn):
+        while True:
+            try:
+                data = conn.recv()
+            except EOFError:
+                return
+            command = data[0].upper()
+            args = data[1]
+            if command == 'SCAN':
+                self.to_scan_condition.acquire()
+                self.to_scan.add(args)
+                self.to_scan_condition.notify()
+                self.to_scan_condition.release()
+            #TODO: STATUS
+            #TODO: SHUTDOWN
+
+class ScannerClient():
+    def __init__(self, connection_info):
+        self.conn = multiprocessing.connection.Client(connection_info[0], authkey=connection_info[1])
+    
+    def scan(self, folder_id):
+        self.conn.send(('SCAN', folder_id))
+        self.conn.close()
