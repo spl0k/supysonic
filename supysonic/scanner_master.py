b062e561ef918183db05ca6fe23712a7d4526443
Taizo Simpson <tts1848@cs.rit.edu>
2018-10-17 21:28:10 -0400

    Added shutdown functionality

--- supysonic/scanner_master.py
+++ supysonic/scanner_master.py
@@ -32,10 +32,12 @@ class ScannerMaster():
         self.to_scan = set()
         self.progress = -1
         self.scan_thread = threading.Thread(target=self._keep_scanning)
+        self.shutting_down = False
+        self.active_connections = set()
     
     def run(self):
         self.scan_thread.start()
-        while True:
+        while not self.shutting_down:
             try:
                 conn = self.listener.accept()
             except multiprocessing.connection.AuthenticationError:
@@ -43,12 +45,27 @@ class ScannerMaster():
             listen_thread = threading.Thread(target=self._listen_for_commands, args=(conn,))
             listen_thread.start()
 
+    def shutdown(self, notify=None):
+        self.shutting_down = True
+        self.listener.close()
+        for conn in self.active_connections:
+            conn.close()
+        self.to_scan_condition.acquire()
+        self.to_scan_condition.notify_all()
+        self.to_scan_condition.release()
+        self.scan_thread.join()
+        if notify:
+            notify.send(None)
+
     def _keep_scanning(self):
-        while True:
+        while not self.shutting_down:
             self.progress = -1
             self.to_scan_condition.acquire()
             while not len(self.to_scan):
                 self.to_scan_condition.wait()
+                if self.shutting_down:
+                    self.to_scan_condition.release()
+                    break
             folder = self.to_scan.pop()
             self.to_scan_condition.release()
             self._scan_folder(folder)
@@ -67,11 +84,13 @@ class ScannerMaster():
         self.progress = progress
     
     def _listen_for_commands(self, conn):
-        while True:
+        self.active_connections.add(conn)
+        while not self.shutting_down:
             try:
                 data = conn.recv()
             except EOFError:
-                return
+                self.active_connections.remove(conn)
+                break
             command = data[0].upper()
             args = data[1:]
             if command == 'SCAN':
@@ -85,8 +104,10 @@ class ScannerMaster():
                     conn.send((True, p))
                 else:
                     conn.send((False, ))
-            #TODO: STATUS
-            #TODO: SHUTDOWN
+            elif command == 'SHUTDOWN':
+                self.active_connections.remove(conn)
+                self.shutdown(notify=conn)
+                conn.close()
 
 class ScannerClient():
     def __init__(self, connection_info):
@@ -99,3 +120,9 @@ class ScannerClient():
         self.conn.send(('STATUS', ))
         return self.conn.recv()
 
+    def shutdown(self):
+        self.conn.send(('SHUTDOWN', ))
+        self.conn.recv()
+
+    def close(self):
+        self.conn.close()
