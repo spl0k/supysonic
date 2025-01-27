ea98ac910b3833e4955d23a10a2a433a3994c4b1
vithyze <127023076+vithyze@users.noreply.github.com>
2025-01-27 23:47:37 +0000

    Update ldap.py

--- supysonic/managers/ldap.py
+++ supysonic/managers/ldap.py
@@ -1,75 +1,64 @@
 import logging
-
-
-import ldap3
-
+try:
+    import ldap3
+except ModuleNotFoundError:
+    ldap3 = None
 
 logger = logging.getLogger(__name__)
 
-
 class LdapManager:
-    def __init__(
-        self,
-        ldap_server,
-        base_dn,
-        user_filter,
-        admin_filter,
-        bind_dn,
-        bind_password,
-        username_attr,
-        email_attr,
-    ):
-        self.ldap_server = ldap_server
-        self.base_dn = base_dn
-        self.user_filter = user_filter
-        self.admin_filter = admin_filter
-        self.bind_dn = bind_dn
-        self.bind_password = bind_password
-        self.username_attr = username_attr
-        self.email_attr = email_attr
-        if not self.ldap_server:
-            raise ValueError("No LDAP configured")
-        self.server = ldap3.Server(self.ldap_server, get_info="ALL")
+    def __init__(self, **config):
+        if not config["server_url"]:
+            raise ValueError('No LDAP Server_url configure ') 
+        elif not ldap3:
+            logger.error("Module 'ldap3' is not installed.")
+            raise ValueError('Module ldap3 is not installed')
+        elif None in config.values():
+            logger.error("Some required LDAP parameters are missing.")
+            raise ValueError('some required LDAP paramters are missing') 
 
-    def try_auth(self, user, password):
+        self.server = ldap3.Server(config["server_url"], get_info=None)
+        self.config = config
+
+    def try_auth(self, username, password):
         admin = False
-        if self.admin_filter:
-            entrie = self.search_user(user, self.admin_filter)
-            if entrie:
-                logger.debug("{0} is admin".format(user))
-                admin = True
-        if not admin:
-            entrie = self.search_user(user, self.user_filter)
-            if not entrie:
-                return False
-        try:
-            with ldap3.Connection(
-                self.server, entrie.entry_dn, password, read_only=True
-            ) as conn:
-                return {
-                    "uid": entrie[self.username_attr],
-                    "mail": entrie[self.email_attr],
-                    "admin": admin,
-                }
-        except ldap3.core.exceptions.LDAPBindError:
-            logger.warning("wrong password for user {0}".format(user))
-            return False
 
-    def search_user(self, user, filter):
+        if self.config["admin_filter"]:
+            entry = self.search_user(username, self.config["admin_filter"])
+            if entry:
+                logger.info(f"User '{username}' is admin.")
+                admin = True
+
+        if not admin:
+            entry = self.search_user(username, self.config["user_filter"])
+
+        if entry:
+            try:
+                with ldap3.Connection(self.server, entry.entry_dn, password, read_only=True):
+                    return {
+                        "mail": entry[self.config["email_attr"]],
+                        "admin": admin
+                    }
+            except ldap3.core.exceptions.LDAPBindError:
+                logger.error(f"Bind failed for '{entry.entry_dn}'.")
+            except Exception as e:
+                logger.error(f"LDAP error: {e}")
+
+    def search_user(self, username, _filter):
         try:
-            with ldap3.Connection(
-                self.server, self.bind_dn, self.bind_password, read_only=True
-            ) as conn:
+            with ldap3.Connection(self.server, self.config["bind_dn"], self.config["bind_pw"], read_only=True) as conn:
                 conn.search(
-                    self.base_dn,
-                    filter,
-                    attributes=[self.email_attr, self.username_attr],
+                    self.config["base_dn"],
+                    _filter.format(username=username),
+                    attributes=[self.config["username_attr"], self.config["email_attr"]],
+                    size_limit=1
                 )
                 entries = conn.entries
+                if entries and entries[0][self.config["username_attr"]] == username:
+                    return entries[0]
+                else:
+                    logger.info(f"User '{username}' not found in LDAP database.")
         except ldap3.core.exceptions.LDAPBindError:
-            logger.warning("wrong can't bind LDAP with {0}".format(self.bind_dn))
-            return False
-        for entrie in entries:
-            if entrie[self.username_attr] == user:
-                return entrie
-        return False
+            logger.error(f"Bind failed for '{self.config['bind_dn']}'.")
+        except Exception as e:
+            logger.error(f"LDAP error: {e}")
